621cecc65aba4b89d8f590566c4f2c59ea3e7885
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2023-04-07 11:43:43 -0300

    Update load_dataset.sh

--- load_dataset.sh
+++ load_dataset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash	
 kaggle datasets download -d gauravtopre/bank-customer-churn-dataset
 unzip -o -p bank-customer-churn-dataset > bank_customer_churn.csv 
-mkdir datasets && mv bank_customer_churn.csv datasets/
+mkdir datasets/ && mv bank_customer_churn.csv datasets/
 	
